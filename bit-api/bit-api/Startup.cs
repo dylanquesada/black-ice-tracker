2ce38b5a79737829e55412bf1d68b1f422e64822
Dylan Quesada <dquesada@nvisia.com>
2021-01-17 01:07:42 -0600

    end-to-end functionality, added typescript, allow CORS on Web API

--- bit-api/bit-api/Startup.cs
+++ bit-api/bit-api/Startup.cs
@@ -19,6 +19,8 @@ namespace bit_api
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,6 +31,16 @@ namespace bit_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
             services.AddDbContext<GunContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging());
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -44,6 +56,7 @@ namespace bit_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -54,16 +67,19 @@ namespace bit_api
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = string.Empty;
             });
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+            //ToDo: what's this? ^
 
             app.UseRouting();
-
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
---
+++ bit-fe/src/services/api/utilities/index.ts
@@ -0,0 +1,1 @@
+import { ApiCore } from "./core";
